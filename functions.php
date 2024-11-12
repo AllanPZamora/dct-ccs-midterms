3bf3fa7ea340428f8082b208289372475ccd78b1
AllanPZamora <gelgelacc1@gmail.com>
2024-11-12 22:37:55 +0800

    added functions in in the directory

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function getUsers() {
+    return [
+        ["email" => "admin1@example.com", "password" => "admin1"],
+        ["email" => "admin2@example.com", "password" => "admin2"],
+        ["email" => "admin3@example.com", "password" => "admin3"],
+        ["email" => "admin4@example.com", "password" => "admin4"],
+        ["email" => "admin5@example.com", "password" => "admin5"]
+    ];
+}
+
+?>
