fc29436b65fb0f08cfda822b03fa2fd6966b9f38
AllanPZamora <gelgelacc1@gmail.com>
2024-11-12 21:40:42 +0800

    created static login

--- index.php
+++ index.php
@@ -3,9 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    
+<body class="d-flex justify-content-center align-items-center vh-100">
+
+<div class="card p-4" style="width: 300px;">
+    <h3 class="card-title text-center">Login</h3>
+    <div class="container">
+        <form>
+
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="text" class="form-control" id="email" name="email" placeholder="Enter email">
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+
+            <button type="submit" class="btn btn-primary w-100">Login</button>
+            
+        </form>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
